6b2cb8bc2ddaf072a3a86600fd6fab398bf3b6cd
solex <solomonyunana95@gmail.com>
2019-09-19 20:22:45 +0100

    Added readme file and updated refs folder

--- README.txt
+++ README.txt
@@ -1,4 +1,20 @@
-The .env file contains credentials and urls used in the app.
-The app uses the doctor key as the channel for communication between patient and doc.
-The app uses keys to relate to each other e.g to get all doctors are categroized under their respectives hostpitals keys in the doctor collection and all donation has the key of doctor respectively 
-some functions or methods are self explanatory
+
+
+The mobile project contains two files i used for abstraction
+=================================================================
+
+1) firebaseRefs.js  // which you can find at "./app/utils/refs.js" for changing firestoreRefs and realTime database refs
+2) strings.js // which you can find at "./app/utils/strings.js" for changing ui values;
+
+
+---------------------------------------------------------------------------------------------------------------
+To change references to database collections to look like model equal to the model of the DB now and to change project this procedures will help
+-----------------------------------------------------------------------------------------------------------------
+
+1) (a) Download the googleservice.json file from the newly created project and replace the existing one on the "android/app/" folder on the project directory => for mobile. NOTE do this before build ;
+
+   (1b) Go to directory of Admin project and at root directory is the firebase.js file were configuration for firebase for web project resides. copy the new project configuration looking like the present one and replace;
+
+2) Make sure you set firestore and Real time database of firebase security rules to allow read and write without auth;
+
+3) To change values of the structure of database collections, subcollection or path in real time database pleas look at each structure in database and change value as you find in refs files. after editing a ref file in either mobile or web copy and paste in the other counterpart since they are all the same.
--- app/Utils/refs.js
+++ app/Utils/refs.js
@@ -1,28 +1,40 @@
 
- const References  = {
-     CategoryOne : "Hospitals",//main collection in database for hospitals see  db
-     CategoryTWo : "doctors",// main collection in database 
-     CategoryThree: "Appointments",// main collection in database 
-     CategoryFour:"Rating", // main collection in database 
-     CategoryFive: "Suggestions",// main collection in database 
-     CategorySix : "charities",// main collection in database 
-     CategorySeven: "users",// main collection in database 
-     CategoryEight: "userRating",// main collection in database 
-     CategoryNine: "AppState",// main collection in database 
-     CategoryTen:"donation",// main collection in database 
-     CateogryEleven: "status",
-     CategoryTwelve: "tempCoins",// main collection in database 
-     CategoryThirteen: "coins",//sub collection in database see db
-     CategoryFourteen: "doctorsDonationTrack",// main collection in database 
-     CategoryFiftenn: "amount",//sub collection or doc see db
-     CategorySixteen: "listeners",//main ref from not firestore db
-     CategorySeventeen: "credentials",//sub collection see db,
-     CategoryEighteen:"personalInfo",
-     CategoryNineteen:"info",
-     CategoryTwenty:"history",
-     CategoryTwentyOne:"credentials",
-     CategoryTwentyTwo:"Reports"
-} 
+
+const $REFS = {
+    
+    CategoryOne : "Hospitals",
+    CategoryTWo : "doctors", 
+    CategoryThree: "Appointments", 
+    CategoryFour:"Rating",  
+    CategoryFive: "Suggestions", 
+    CategorySix : "charities", 
+    CategorySeven: "users", 
+    CategoryEight: "userRating", 
+    CategoryNine: "AppState", 
+    CategoryTen:"donation", 
+    CateogryEleven: "status",
+    CategoryTwelve: "tempCoins", 
+    CategoryThirteen: "coins",
+    CategoryFourteen: "doctorsDonationTrack", 
+    CategoryFiftenn: "amount",
+    CategorySixteen: "listeners",
+    CategorySeventeen: "credentials",
+    CategoryEighteen:"personalInfo",
+    CategoryNineteen:"info",
+    CategoryTwenty:"history",
+    CategoryTwentyOne:"credentials",
+    CategoryTwentyTwo:"Reports",
+    CategoryTwentyThree:'appointments',
+    CategoryTwentyFour:"doctorAppointment",
+    CategoryTwentyFive:"personalInfo",
+    CategoryTwentySix:"info",
 
 
-export default References 
+
+    
+
+}
+
+$REFS.CategoryTwentyOne = $REFS.CategorySeventeen
+
+export default $REFS;
